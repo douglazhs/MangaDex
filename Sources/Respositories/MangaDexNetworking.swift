86f61ed62459cd3d1abd7376baf5f6f3765bfee7
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2024-06-04 18:38:34 -0300

    fix: fixed cover list request limit

--- Sources/Respositories/MangaDexNetworking.swift
+++ Sources/Respositories/MangaDexNetworking.swift
@@ -40,7 +40,7 @@ extension MangaDexNetworking: TargetType {
         case .cover(let id, let fileName):
             return "/covers/\(id)/\(fileName)"
         case .coverList(let id):
-            return "/cover?manga[]=\(id)&order[volume]=desc&limit=200"
+            return "/cover?manga[]=\(id)&order[volume]=desc&limit=100"
         case .feed(let id, _):
             return "/manga/\(id)/feed?includes[]=scanlation_group"
         case .chapter(let id):
